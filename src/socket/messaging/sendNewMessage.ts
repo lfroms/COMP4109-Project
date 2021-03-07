7be14e8da130ae8b3cbf4118fe9737e80288b836
Khaled W. Mohamed <59355319+khaledWM@users.noreply.github.com>
2021-03-07 18:26:11 -0500

    Update src/socket/messaging/sendNewMessage.ts
    Co-authored-by: Lukas Romsicki <3951690+lfroms@users.noreply.github.com>

--- src/socket/messaging/sendNewMessage.ts
+++ src/socket/messaging/sendNewMessage.ts
@@ -2,7 +2,7 @@ import { Server } from 'socket.io';
 
 export default function sendNewMessage(io: Server) {
   io.on('connection', socket => {
-    socket.on('broadcast_message', function (data: string) {
+    socket.on('broadcast_message', (data: string) => {
       io.emit('broadcast_message', data);
     });
   });
