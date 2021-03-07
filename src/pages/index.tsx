248d9576fd43bc5ecd84b31f83b2bddc04fb48cf
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-07 17:40:47 -0500

    10-Initial-View
    Created (very) basic initial page with textbox and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useConnectToSocketOnce } from 'hooks';
 
-export default function App() {
+export default function Index() {
   useConnectToSocketOnce();
 
-  return <p>Hello, World!</p>;
+  const [chatID, setChatID] = useState('');
+
+  const inputText = (
+    <input id="chatID" type="text" onChange={elem=>setChatID(elem.currentTarget.value)} value={chatID}/>
+  );
+
+  const joinButton = (
+    <button onClick={joinRoom}>
+      Join
+    </button>
+  );
+
+  function joinRoom (){
+    setChatID('');
+  }
+
+  return (
+    <div>
+      <h1>Welcome to CryptoChat!</h1>
+      <div>
+        <p>
+          Enter the room to join below:
+        </p>
+        {inputText}
+        {joinButton}
+      </div>
+    </div>
+  );
 }
