18ae1f997d30207a23d2ab6696e70b67464e0e3f
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-07 22:52:21 -0500

    Create basic chat ui

---
+++ src/pages/chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useConnectToSocketOnce } from 'hooks';
+
+export default function Chat() {
+  useConnectToSocketOnce();
+
+  return (
+  <div>
+    <h1>Welcome to the generic chat page!</h1>
+    <p>You might want to add a chat ID so you get into a room (Ex: ../chat/'someValue')</p>
+  </div>
+  );
+}
---
+++ src/pages/chat/[chatID].tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useConnectToSocketOnce } from 'hooks';
+import { useRouter } from 'next/router'
+
+export default function Chat() {
+  useConnectToSocketOnce();
+
+  const router = useRouter();
+  const { chatID } = router.query;
+
+  const [message, setMessage] = useState('');
+  const [conversationText, updateConversation] = useState(["Message 1", "Message 2", "Message 3"]);
+
+  const messageInput = (
+    <input
+      id="msgInput"
+      type="text"
+      onChange={elem => setMessage(elem.currentTarget.value)}
+      placeholder="Type a message..."
+      value={message}
+    />
+  );
+
+  const sendButton = <button onClick={sendMessage}>Send</button>;
+
+  function sendMessage (){
+    conversationText.push(message);
+    updateConversation(conversationText);
+    setMessage('');
+  }
+
+  return (
+  <div>
+    <h1>Welcome to a Chatroom {chatID}!</h1>
+    <div id={"conversation" + {chatID}}>
+      {conversationText.map((text) =>
+        <p>{text}</p>
+      )}
+    </div>
+    {messageInput}
+    {sendButton}
+  </div>
+  );
+}
