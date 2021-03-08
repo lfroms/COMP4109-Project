311eb073d65a832f44cb74e6c01dd0dff2d5abe4
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-08 14:39:31 -0500

    Create basic chat ui (#24)
    * Create basic chat ui
    
    * Updating formating for ESLint
    
    * Changed messages to objects to store msgID
    
    Helps when generating the HTML and passing ESLint testing
    
    * Last update to fix formatting
    
    * Formatting changes
    
    * ESLint is getting on my nerves :(
    
    * Remove /chat page, removed counter and updated variable names
    
    * Update [chatID].tsx
    
    * Rename [chatID].tsx to [chatId].tsx

---
+++ src/pages/chat/[chatId].tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Chat() {
+  const router = useRouter();
+  const { chatId } = router.query;
+
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([
+    { data: 'Message 1' },
+    { data: 'Message 2' },
+    { data: 'Message 3' },
+  ]);
+
+  const messageInput = (
+    <input
+      id="msgInput"
+      type="text"
+      onChange={elem => setMessage(elem.currentTarget.value)}
+      placeholder="Type a message..."
+      value={message}
+    />
+  );
+
+  const sendButton = <button onClick={sendMessage}>Send</button>;
+
+  function sendMessage() {
+    messages.push({ data: message });
+    setMessages(messages);
+    setMessage('');
+  }
+
+  return (
+    <div>
+      <h1>Welcome to a Chatroom {chatId}!</h1>
+      {messages.map((message, index) => (
+        <p key={`message-${index}`}>{message.data}</p>
+      ))}
+      {messageInput}
+      {sendButton}
+    </div>
+  );
+}
