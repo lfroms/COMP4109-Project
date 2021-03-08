c8dbb179d30ad8896911bb0c71d44b7887a7bd48
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-08 13:52:58 -0500

    Remove /chat page, removed counter and updated variable names

--- src/pages/chat/[chatID].tsx
+++ src/pages/chat/[chatID].tsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react';
-import { useConnectToSocketOnce } from 'hooks';
 import { useRouter } from 'next/router';
 
 export default function Chat() {
-  useConnectToSocketOnce();
 
   const router = useRouter();
-  const { chatID } = router.query;
+  const { chatId } = router.query;
 
-  const [count, setCount] = useState(3);
   const [message, setMessage] = useState('');
-  const [conversationText, updateConversation] = useState([
-    { id: 1, data: 'Message 1' },
-    { id: 2, data: 'Message 2' },
-    { id: 3, data: 'Message 3' },
+  const [messages, setMessages] = useState([
+    { data: 'Message 1' },
+    { data: 'Message 2' },
+    { data: 'Message 3' },
   ]);
 
   const messageInput = (
@@ -29,20 +26,17 @@ export default function Chat() {
   const sendButton = <button onClick={sendMessage}>Send</button>;
 
   function sendMessage() {
-    conversationText.push({ id: count, data: message });
-    updateConversation(conversationText);
+    messages.push({ data: message });
+    setMessages(messages);
     setMessage('');
-    setCount(count + 1);
   }
 
   return (
     <div>
-      <h1>Welcome to a Chatroom {chatID}!</h1>
-      <div id={'conversation' + { chatID }}>
-        {conversationText.map(text => (
-          <p key={text.id}>{text.data}</p>
-        ))}
-      </div>
+      <h1>Welcome to a Chatroom {chatId}!</h1>
+      {messages.map((message, index) => (
+        <p key={`message-${index}`}>{message.data}</p>
+      ))}
       {messageInput}
       {sendButton}
     </div>
