7b958fff9321a3c7e8f5e585d800e2e907f3fdd3
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-08 09:38:57 -0500

    Changed messages to objects to store msgID
    Helps when generating the HTML and passing ESLint testing

--- src/pages/chat/[chatID].tsx
+++ src/pages/chat/[chatID].tsx
@@ -8,8 +8,9 @@ export default function Chat() {
   const router = useRouter();
   const { chatID } = router.query;
 
+  const [count, setCount] = useState(3);
   const [message, setMessage] = useState('');
-  const [conversationText, updateConversation] = useState(['Message 1', 'Message 2', 'Message 3']);
+  const [conversationText, updateConversation] = useState([{id: 1, data: 'Message 1'}, {id: 2, data: 'Message 2'}, {id: 3, data: 'Message 3'}]);
 
   const messageInput = (
     <input
@@ -24,16 +25,19 @@ export default function Chat() {
   const sendButton = <button onClick={sendMessage}>Send</button>;
 
   function sendMessage() {
-    conversationText.push(message);
+    conversationText.push({id: count, data: message});
     updateConversation(conversationText);
     setMessage('');
+    setCount(count + 1);
   }
 
   return (
     <div>
       <h1>Welcome to a Chatroom {chatID}!</h1>
-      <div id={"conversation" + { chatID }}>
-        {conversationText.map((text) => <p>{text}</p>)}
+      <div id={'conversation' + { chatID }}>
+        {conversationText.map(text => (
+          <p key={text.id}>{text.data}</p>
+        ))}
       </div>
       {messageInput}
       {sendButton}
