0a93021bb15409c381bd5563a2cf06c8b209d3b8
Lukas Romsicki <lukas@romsicki.com>
2021-03-16 22:37:32 -0400

    Convert AsymmetricEncryptionService to use CryptoKey

--- src/services/asymmetricEncryptionService.ts
+++ src/services/asymmetricEncryptionService.ts
@@ -1,36 +1,36 @@
 import { BaseEncryptionService } from './base';
 
 export default class AsymmetricEncryptionService extends BaseEncryptionService {
-  public async encrypt(plaintext: string, publicKey: JsonWebKey) {
+  public async encrypt(plaintext: string, publicKey: CryptoKey) {
     const textEncoder = new TextEncoder();
 
     const ciphertext = await window.crypto.subtle.encrypt(
       {
         name: 'RSA-OAEP',
       },
-      await this.convertJsonWebKeyToCryptoKey(publicKey, 'encrypt'),
+      publicKey,
       textEncoder.encode(plaintext)
     );
 
     return this.arrayBufferToString(ciphertext);
   }
 
-  public async decrypt(ciphertext: string, privateKey: JsonWebKey) {
+  public async decrypt(ciphertext: string, privateKey: CryptoKey) {
     const textDecoder = new TextDecoder();
 
     const plaintext = await window.crypto.subtle.decrypt(
       {
         name: 'RSA-OAEP',
       },
-      await this.convertJsonWebKeyToCryptoKey(privateKey, 'decrypt'),
+      privateKey,
       super.stringToArrayBuffer(ciphertext)
     );
 
     return textDecoder.decode(plaintext);
   }
 
-  public static async generateKeyPair(): Promise<JsonWebKeyPair> {
-    const keyPair = await window.crypto.subtle.generateKey(
+  public static async generateKeyPair() {
+    return window.crypto.subtle.generateKey(
       {
         name: 'RSA-OAEP',
         modulusLength: 4096,
@@ -39,24 +39,6 @@ export default class AsymmetricEncryptionService extends BaseEncryptionService {
       },
       true,
       ['encrypt', 'decrypt']
-    );
-
-    return {
-      publicKey: await window.crypto.subtle.exportKey('jwk', keyPair.publicKey),
-      privateKey: await window.crypto.subtle.exportKey('jwk', keyPair.privateKey),
-    };
-  }
-
-  private convertJsonWebKeyToCryptoKey(keyData: JsonWebKey, mode: 'encrypt' | 'decrypt') {
-    return window.crypto.subtle.importKey(
-      'jwk',
-      keyData,
-      {
-        name: 'RSA-OAEP',
-        hash: 'SHA-256',
-      },
-      false,
-      [mode]
     );
   }
 }
