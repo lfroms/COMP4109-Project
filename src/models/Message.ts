019d3a0bbac918f20f87afa40af274496aea1fb4
Lukas Romsicki <lukas@romsicki.com>
2021-03-25 17:58:59 -0400

    Add sender to messages

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,10 +1,15 @@
-import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Conversation } from './Conversation';
+import { User } from './User';
 
 @Entity()
 export class Message extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn()
+  sender: User;
 
   @ManyToOne(() => Conversation, conversation => conversation.messages)
   conversation: Conversation;
