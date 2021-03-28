178a52ff7e72f148a400a939f2ddf28de9675a7b
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-27 21:20:12 -0400

    I was dumb, simplifying the checks

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -15,33 +15,7 @@ type Response = API.JSONResponse<API.UsersResponse>;
 router.get<any, Response, any, Request>('/api/users', async (request, response) => {
   const { ids } = request.query;
 
-  if (!ids) {
-    const users = await User.find();
-
-    return response.json({
-      data: {
-        users: users.map(user => ({
-          id: user.id,
-          name: user.name,
-          username: user.username,
-          publicKey: user.publicKey,
-        })),
-      },
-      error: null,
-    });
-  }
-
-  if (!ids.split(',')) {
-    return response.status(400).json({
-      data: null,
-      error: {
-        code: 400,
-        message: 'Malformed query parameters',
-      },
-    });
-  }
-
-  const users = await User.findByIds(ids.split(','));
+  const users = ids ? await User.findByIds(ids.split(',')) : await User.find();
 
   return response.json({
     data: {
