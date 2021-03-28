d57547257a0cbfc9f81697f82ec38da2577673b4
Robert Irwin <63419833+atlasrevenge@users.noreply.github.com>
2021-03-27 21:22:56 -0400

    Covering issues of improper formatting

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -15,6 +15,16 @@ type Response = API.JSONResponse<API.UsersResponse>;
 router.get<any, Response, any, Request>('/api/users', async (request, response) => {
   const { ids } = request.query;
 
+  if (ids && !ids.split(',')) {
+    return response.status(400).json({
+      data: null,
+      error: {
+        code: 400,
+        message: 'Malformed query parameters',
+      },
+    });
+  }
+
   const users = ids ? await User.findByIds(ids.split(',')) : await User.find();
 
   return response.json({
